76482f34c9415c5d05647948f187dc632a6714d5
Frida Wallgren <58770144+fridawa@users.noreply.github.com>
2021-10-11 12:57:20 +0200

    map working on searchresults

--- src/components/List.js
+++ src/components/List.js
@@ -4,18 +4,43 @@ const List = ({ hotels }) => {
   return (
     <>
       {hotels.map((hotel) => {
-        const { id, name, description, rating, rooms } = hotel;
+        const {
+          id,
+          name,
+          description,
+          rating,
+          rooms,
+          locationFacts,
+          adress,
+          location,
+          price,
+        } = hotel;
+
+        // const roomList = rooms ? (
+        //   rooms.map((room) => <div>{room}</div>)
+        // ) : (
+        //   <div></div>
+        // );
+
         return (
           <article className="col-md-4" key={id}>
             <div>
               <h4>{name}</h4>
+              <h5>{adress}</h5>
+              <h5>{location}</h5>
               <p>{description}</p>
               <p>{rating}</p>
-
-              {rooms.map((r) => {
-                console.log(r.firstRoom);
-                return <div>{r}</div>;
+              <p>{locationFacts.join(" ")} </p>
+              {rooms.map((room) => {
+                console.log(room);
+                return (
+                  <>
+                    <p>{room.roomType}</p>
+                  </>
+                );
               })}
+              <p>{price}</p>
+              <button>Book Now</button>
             </div>
           </article>
         );
--- src/components/data.js
+++ src/components/data.js
@@ -11,21 +11,19 @@ export default [
     ],
     rooms: [
       {
-        firstRoom: {
-          roomType: "Two single bed room",
-          spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
-          accessibility: ["Wheelchair friendly, Room at ground level"],
-        },
-        secondRoom: {
-          roomType: "Comfort Room",
-          spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
-          accessibility: ["Wheelchair friendly, Room at ground level"],
-        },
-        thirdRoom: {
-          roomType: "Deluxe Room",
-          spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
-          accessibility: ["Wheelchair friendly, Room at ground level"],
-        },
+        roomType: "Two single bed room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Comfort Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Deluxe Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
       },
     ],
     facilites: [
@@ -73,40 +71,345 @@ export default [
   {
     id: 2,
     name: "Hotel two",
+    adress: "363 Barca street",
+    location: "Barcelona",
+    locationFacts: [
+      "2.3 km to the beach",
+      "0.2 km to the subway",
+      "0.1 km to the city",
+    ],
+    rooms: [
+      {
+        roomType: "Two single bed room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Comfort Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Deluxe Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+    ],
+    facilites: [
+      {
+        services: [
+          "Reception open 24/7",
+          "Bagage storage available",
+          "Laundry facilities",
+        ],
+        food: [
+          "Breakfast is served everyday from 6 am to 11 am",
+          "Lunch restaurant open 11 am to 15 pm.",
+          "Dinner restaurant open 4:30 am to late",
+          "Room service 24/7",
+        ],
+        toDo: [
+          "Gym center at the facilities",
+          "Spa with 5 different pools, massage treatments, aroma therapy etc.",
+        ],
+        accessibility: [
+          "Wheelchair friendly rooms ",
+          "Room at ground level accessible",
+          "Elevator to the other floors",
+        ],
+        other: [
+          "TV at the rooms and inte the common areas",
+          "Library",
+          "Terrace on the roof with bar open 12 am to late",
+        ],
+      },
+    ],
+    faclitiesShort: ["Bar", "Pool", "Restaurant", "Gym", "Spa"],
     description:
-      "Offering a California-eclectic menu and signature cocktails, H2 Kitchen & Bar is located at Hotel Loews Hollywood. The restaurant prepares entrees using locally-grown products and farm fresh ingredients. There is a quick grab-and-go market, with artisan pastries, pressed juices, and luxury candies and chocolates.",
+      "Guests of Loews Hollywood are greeted by a 24-hour reception. For convenience, the hotel offers a 24-hour self-service business centre with computers, copy machines, fax machines and 24-hour concierge services. A fitness centre and yoga room are featured on the fifth floor and offer views of the surrounding city.",
+    rating: 1.2,
+    reviews: [
+      "I loved this hotel. Helpful staff. Great food, really good that all was included. The hotel was also located in the middle of the city which was good for me and my family since we wanted to do a lot of city-activities. Would definitely recommend and visit again sometime.",
+      "Overall a good visit. We stayed in the Deluxe room and had a really good time in the city. Also really nice dinner at the hotel restaurant.  Didn’t have time to check out the gym",
+      "Got this trip as a gift from my family and this hotel really made the whole trip so much more enjoyable. Good and big breakfast. Really helpful staff that showed us some of the citys hidden gems. Would love to stay here again sometime.",
+    ],
+    price: "$ 250",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 3,
     name: "Hotel three",
+    adress: "363 Barca street",
+    location: "Barcelona",
+    locationFacts: [
+      "2.3 km to the beach",
+      "0.2 km to the subway",
+      "0.1 km to the city",
+    ],
+    rooms: [
+      {
+        roomType: "Two single bed room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Comfort Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Deluxe Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+    ],
+    facilites: [
+      {
+        services: [
+          "Reception open 24/7",
+          "Bagage storage available",
+          "Laundry facilities",
+        ],
+        food: [
+          "Breakfast is served everyday from 6 am to 11 am",
+          "Lunch restaurant open 11 am to 15 pm.",
+          "Dinner restaurant open 4:30 am to late",
+          "Room service 24/7",
+        ],
+        toDo: [
+          "Gym center at the facilities",
+          "Spa with 5 different pools, massage treatments, aroma therapy etc.",
+        ],
+        accessibility: [
+          "Wheelchair friendly rooms ",
+          "Room at ground level accessible",
+          "Elevator to the other floors",
+        ],
+        other: [
+          "TV at the rooms and inte the common areas",
+          "Library",
+          "Terrace on the roof with bar open 12 am to late",
+        ],
+      },
+    ],
+    faclitiesShort: ["Bar", "Pool", "Restaurant", "Gym", "Spa"],
     description:
-      "The Hollywood Walk of Fame is less than 1 minutes’ walk from this hotel. The Hollywood Bowl is 6 minutes’ drive away. Los Angeles International Airport is 40.2 km away.",
+      "Guests of Loews Hollywood are greeted by a 24-hour reception. For convenience, the hotel offers a 24-hour self-service business centre with computers, copy machines, fax machines and 24-hour concierge services. A fitness centre and yoga room are featured on the fifth floor and offer views of the surrounding city.",
+    rating: 1.2,
+    reviews: [
+      "I loved this hotel. Helpful staff. Great food, really good that all was included. The hotel was also located in the middle of the city which was good for me and my family since we wanted to do a lot of city-activities. Would definitely recommend and visit again sometime.",
+      "Overall a good visit. We stayed in the Deluxe room and had a really good time in the city. Also really nice dinner at the hotel restaurant.  Didn’t have time to check out the gym",
+      "Got this trip as a gift from my family and this hotel really made the whole trip so much more enjoyable. Good and big breakfast. Really helpful staff that showed us some of the citys hidden gems. Would love to stay here again sometime.",
+    ],
+    price: "$ 250",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 4,
     name: "Hotel four",
+    adress: "363 Barca street",
+    location: "Barcelona",
+    locationFacts: [
+      "2.3 km to the beach",
+      "0.2 km to the subway",
+      "0.1 km to the city",
+    ],
+    rooms: [
+      {
+        roomType: "Two single bed room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Comfort Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Deluxe Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+    ],
+    facilites: [
+      {
+        services: [
+          "Reception open 24/7",
+          "Bagage storage available",
+          "Laundry facilities",
+        ],
+        food: [
+          "Breakfast is served everyday from 6 am to 11 am",
+          "Lunch restaurant open 11 am to 15 pm.",
+          "Dinner restaurant open 4:30 am to late",
+          "Room service 24/7",
+        ],
+        toDo: [
+          "Gym center at the facilities",
+          "Spa with 5 different pools, massage treatments, aroma therapy etc.",
+        ],
+        accessibility: [
+          "Wheelchair friendly rooms ",
+          "Room at ground level accessible",
+          "Elevator to the other floors",
+        ],
+        other: [
+          "TV at the rooms and inte the common areas",
+          "Library",
+          "Terrace on the roof with bar open 12 am to late",
+        ],
+      },
+    ],
+    faclitiesShort: ["Bar", "Pool", "Restaurant", "Gym", "Spa"],
     description:
-      "A flat-screen cable TV with pay-per-view channels is provided in each guest room at Loews Hollywood Hotel. All of the stylish rooms include a work desk, a minibar and a coffee maker supplied with gourmet coffee and tea. Select rooms offer views of the Hollywood Hills.",
+      "Guests of Loews Hollywood are greeted by a 24-hour reception. For convenience, the hotel offers a 24-hour self-service business centre with computers, copy machines, fax machines and 24-hour concierge services. A fitness centre and yoga room are featured on the fifth floor and offer views of the surrounding city.",
+    rating: 1.2,
+    reviews: [
+      "I loved this hotel. Helpful staff. Great food, really good that all was included. The hotel was also located in the middle of the city which was good for me and my family since we wanted to do a lot of city-activities. Would definitely recommend and visit again sometime.",
+      "Overall a good visit. We stayed in the Deluxe room and had a really good time in the city. Also really nice dinner at the hotel restaurant.  Didn’t have time to check out the gym",
+      "Got this trip as a gift from my family and this hotel really made the whole trip so much more enjoyable. Good and big breakfast. Really helpful staff that showed us some of the citys hidden gems. Would love to stay here again sometime.",
+    ],
+    price: "$ 250",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 5,
     name: "Hotel five",
+    adress: "363 Barca street",
+    location: "Barcelona",
+    locationFacts: [
+      "2.3 km to the beach",
+      "0.2 km to the subway",
+      "0.1 km to the city",
+    ],
+    rooms: [
+      {
+        roomType: "Two single bed room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Comfort Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Deluxe Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+    ],
+    facilites: [
+      {
+        services: [
+          "Reception open 24/7",
+          "Bagage storage available",
+          "Laundry facilities",
+        ],
+        food: [
+          "Breakfast is served everyday from 6 am to 11 am",
+          "Lunch restaurant open 11 am to 15 pm.",
+          "Dinner restaurant open 4:30 am to late",
+          "Room service 24/7",
+        ],
+        toDo: [
+          "Gym center at the facilities",
+          "Spa with 5 different pools, massage treatments, aroma therapy etc.",
+        ],
+        accessibility: [
+          "Wheelchair friendly rooms ",
+          "Room at ground level accessible",
+          "Elevator to the other floors",
+        ],
+        other: [
+          "TV at the rooms and inte the common areas",
+          "Library",
+          "Terrace on the roof with bar open 12 am to late",
+        ],
+      },
+    ],
+    faclitiesShort: ["Bar", "Pool", "Restaurant", "Gym", "Spa"],
     description:
-      "A flat-screen cable TV with pay-per-view channels is provided in each guest room at Loews Hollywood Hotel. All of the stylish rooms include a work desk, a minibar and a coffee maker supplied with gourmet coffee and tea. Select rooms offer views of the Hollywood Hills.",
+      "Guests of Loews Hollywood are greeted by a 24-hour reception. For convenience, the hotel offers a 24-hour self-service business centre with computers, copy machines, fax machines and 24-hour concierge services. A fitness centre and yoga room are featured on the fifth floor and offer views of the surrounding city.",
+    rating: 1.2,
+    reviews: [
+      "I loved this hotel. Helpful staff. Great food, really good that all was included. The hotel was also located in the middle of the city which was good for me and my family since we wanted to do a lot of city-activities. Would definitely recommend and visit again sometime.",
+      "Overall a good visit. We stayed in the Deluxe room and had a really good time in the city. Also really nice dinner at the hotel restaurant.  Didn’t have time to check out the gym",
+      "Got this trip as a gift from my family and this hotel really made the whole trip so much more enjoyable. Good and big breakfast. Really helpful staff that showed us some of the citys hidden gems. Would love to stay here again sometime.",
+    ],
+    price: "$ 250",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 6,
     name: "Hotel six",
+    adress: "363 Barca street",
+    location: "Barcelona",
+    locationFacts: [
+      "2.3 km to the beach",
+      "0.2 km to the subway",
+      "0.1 km to the city",
+    ],
+    rooms: [
+      {
+        roomType: "Two single bed room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Comfort Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+      {
+        roomType: "Deluxe Room",
+        spec: ["Two single beds", "2 adults, 1 child", "20 m²", "Free wifi"],
+        accessibility: ["Wheelchair friendly, Room at ground level"],
+      },
+    ],
+    facilites: [
+      {
+        services: [
+          "Reception open 24/7",
+          "Bagage storage available",
+          "Laundry facilities",
+        ],
+        food: [
+          "Breakfast is served everyday from 6 am to 11 am",
+          "Lunch restaurant open 11 am to 15 pm.",
+          "Dinner restaurant open 4:30 am to late",
+          "Room service 24/7",
+        ],
+        toDo: [
+          "Gym center at the facilities",
+          "Spa with 5 different pools, massage treatments, aroma therapy etc.",
+        ],
+        accessibility: [
+          "Wheelchair friendly rooms ",
+          "Room at ground level accessible",
+          "Elevator to the other floors",
+        ],
+        other: [
+          "TV at the rooms and inte the common areas",
+          "Library",
+          "Terrace on the roof with bar open 12 am to late",
+        ],
+      },
+    ],
+    faclitiesShort: ["Bar", "Pool", "Restaurant", "Gym", "Spa"],
     description:
-      "A flat-screen cable TV with pay-per-view channels is provided in each guest room at Loews Hollywood Hotel. All of the stylish rooms include a work desk, a minibar and a coffee maker supplied with gourmet coffee and tea. Select rooms offer views of the Hollywood Hills.",
+      "Guests of Loews Hollywood are greeted by a 24-hour reception. For convenience, the hotel offers a 24-hour self-service business centre with computers, copy machines, fax machines and 24-hour concierge services. A fitness centre and yoga room are featured on the fifth floor and offer views of the surrounding city.",
+    rating: 1.2,
+    reviews: [
+      "I loved this hotel. Helpful staff. Great food, really good that all was included. The hotel was also located in the middle of the city which was good for me and my family since we wanted to do a lot of city-activities. Would definitely recommend and visit again sometime.",
+      "Overall a good visit. We stayed in the Deluxe room and had a really good time in the city. Also really nice dinner at the hotel restaurant.  Didn’t have time to check out the gym",
+      "Got this trip as a gift from my family and this hotel really made the whole trip so much more enjoyable. Good and big breakfast. Really helpful staff that showed us some of the citys hidden gems. Would love to stay here again sometime.",
+    ],
+    price: "$ 250",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
